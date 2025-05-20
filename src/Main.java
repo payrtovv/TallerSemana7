b5a7cd974b0b28f0321608bc384b9fc1b7b436f9
Payrt <payrtovv@gmail.com>
2025-05-19 19:53:21 -0500

    2.0

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
         Avenger avenger = new Avenger(123, "IronMan", new Mision("Derrotar a nisequien", 5), 10000);
 
+        Avenger avenger2 = new Avenger(12423, "dsasda", new Mision("dsad", 5), 321321);
+
+
         System.out.println(avenger.toString());
 
         System.out.println(avenger.CalcularFondoHeroes());
         System.out.println(avenger.calcularImpuesto());
+
+        ArrayList<Avenger> listaAvengers = new ArrayList<>();
+
+
+        listaAvengers.add(avenger);
+        listaAvengers.add(avenger2);
+
+        for (Avenger avenger1: listaAvengers){
+            System.out.println(avenger1.toString());
+        }
+
     }
 }
